503b8b58ca075810d71c77c5e9a97bd31a23d162
NamSam <namitasam05@gmail.com>
2025-03-01 15:09:05 +0530

    Add files via upload

---
+++ DetectAndRecognizeNumberPlate/main.py
@@ -0,0 +1,26 @@
+import cv2
+
+hercescad = "haarcascade_russian_plate_number.xml"
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img=cap.read()
+
+    plate_cascade = cv2.CascadeClassifier(hercescad)
+
+    img_gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    plates= plate_cascade.detectMultiScale(img_gray, 1.1,4)
+    # contains all rectangular points(x,y,w,h)
+
+    for(x,y,w,h) in plates:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+    cv2.imshow("car Plate",img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
